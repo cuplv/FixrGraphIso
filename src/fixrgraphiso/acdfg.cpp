23fb6b1961a7f5854faaf87622b8bd2683a4b3de
smover <sergio.mover@gmail.com>
2016-06-08 23:49:56 -0600

    Add data structure for the acdfg

---
+++ src/fixrgraphiso/acdfg.cpp
@@ -0,0 +1,107 @@
+// -*- C++ -*-
+//
+// Data structure used to store an Abstract Control Data Flow Graph ACDFG)
+//
+// Author: Sergio Mover
+//
+
+#include "fixrgraphiso/acdfg.h"
+
+namespace fixrgraphiso {
+
+//------------------------------------------------------------------------------
+// Implementation of the nodes
+//------------------------------------------------------------------------------
+
+Node::Node(long id)
+{
+  this->id_ = id;
+}
+
+long Node::get_id()
+{
+  return id_;
+}
+
+DataNode::DataNode(long id, const string& name,
+                   const string& data_type) : Node(id)
+{
+  this->name_ = name;
+  this->data_type_ = data_type;
+}
+
+const string& DataNode::get_name()
+{
+  return name_;
+}
+
+const string& DataNode::get_data_type()
+{
+  return data_type_;
+}
+
+CommandNode::CommandNode(long id) : Node(id) {};
+
+MethodNode::MethodNode(long id, const string& name,
+                       const DataNode* receiver,
+                       std::vector<DataNode*> arguments) : CommandNode(id)
+{
+  name_ = name;
+  receiver_ = receiver_;
+  arguments_ = arguments;
+}
+
+const string& MethodNode::get_name()
+{
+  return name_;
+}
+
+const DataNode* MethodNode::get_receiver()
+{
+  return receiver_;
+}
+
+const std::vector<DataNode*> MethodNode::get_arguments()
+{
+  return arguments_;
+}
+
+//------------------------------------------------------------------------------
+// Implementation of the edges
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Implementation of the graph
+//------------------------------------------------------------------------------
+
+void Acdfg::add_node(Node node)
+{
+  nodes_.push_back(node);
+}
+
+void Acdfg::add_edge(Edge edge)
+{
+  edges_.push_back(edge);
+}
+
+nodes_t::const_iterator Acdfg::begin_nodes()
+{
+  return nodes_.begin();
+}
+
+nodes_t::const_iterator Acdfg::end_nodes()
+{
+  return nodes_.end();
+}
+
+edges_t::const_iterator Acdfg::begin_edges()
+{
+  return edges_.begin();
+}
+
+edges_t::const_iterator Acdfg::end_edges()
+{
+  return edges_.end();
+}
+
+} // namespace fixrgraphiso
---
+++ src/fixrgraphiso/acdfg.h
@@ -0,0 +1,109 @@
+// -*- C++ -*-
+//
+// Data structure used to store an Abstract Control Data Flow Graph ACDFG)
+//
+// Author: Sergio Mover
+//
+
+#ifndef ACDFG_H_INCLUDED
+#define ACDFG_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+
+namespace fixrgraphiso {
+using std::string;
+
+// Represent a node in the graph
+class Node {
+public:
+  Node() {};
+  Node(long id);
+  long get_id();
+
+private:
+  long id_;
+};
+
+// Node that represent a data (e.g variable)
+class DataNode : public Node {
+public:
+  DataNode(long id, const string& name, const string& data_type);
+  const string& get_name();
+  const string& get_data_type();
+
+private:
+  string name_;
+  string data_type_;
+};
+
+// Base class for control nodes
+class CommandNode : public Node {
+public:
+  CommandNode(long id);
+};
+
+
+// Represent a method invocation
+class MethodNode : public CommandNode {
+public:
+  MethodNode(long id, const string& name,
+             const DataNode* receiver,
+             std::vector<DataNode*> arguments);
+
+  const string& get_name();
+  const DataNode* get_receiver();
+  const std::vector<DataNode*> get_arguments();
+
+private:
+  // Name of the method
+  string name_;
+  // Id of the receiver of the method
+  DataNode* receiver_;
+  // Parameters passed to the method invocation
+  std::vector<DataNode*> arguments_;
+};
+
+// Represent an edge of the Acdfg
+class Edge {
+public:
+  Edge(long id, Node* src, Node* dst) {id_ = id; src_ = src; dst_ = dst;};
+
+private:
+  long id_;
+  // Src node
+  Node* src_;
+  // Dst node
+  Node* dst_;
+};
+
+class DefEdge : public Edge {};
+class UseEdge : public Edge {};
+class ControlEdge : public Edge {};
+
+typedef std::vector<Node> nodes_t;
+typedef std::vector<Edge> edges_t;
+
+class Acdfg {
+public:
+  Acdfg();
+
+  void add_node(Node node);
+  void add_edge(Edge edge);
+
+  nodes_t::const_iterator begin_nodes();
+  nodes_t::const_iterator end_nodes();
+
+  edges_t::const_iterator begin_edges();
+  edges_t::const_iterator end_edges();
+
+private:
+  nodes_t nodes_;
+  edges_t edges_;
+};
+
+
+} // namespace fixrgraphiso
+
+#endif // ACDFG_H_INCLUDED
