67472221c66e3fa752e6b22541af9a76cd934bac
smover <sergio.mover@gmail.com>
2016-06-13 17:16:15 -0600

    Add precise isomorphism check (still not tested)

--- src/fixrgraphiso/iso.cpp
+++ src/fixrgraphiso/iso.cpp
@@ -15,9 +15,32 @@ namespace fixrgraphiso {
 
 bool Iso::is_iso()
 {
-  z3::expr iso_encoding = z3_context.bool_val(true);
-  z3::solver s(z3_context);
+  z3::solver solver(z3_context);
 
+  std::vector<z3::expr> nodes_iso;
+  std::vector<z3::expr> edges_iso;
+
+  // Get the conjuncts of the encodings
+  get_encoding(nodes_iso, edges_iso);
+
+  // assert the conjuncts
+  for (std::vector<z3::expr>::const_iterator it = nodes_iso.begin();
+       it != nodes_iso.end(); ++it) {
+    z3::expr expr = *it;
+    solver.add(expr);
+  }
+  for (std::vector<z3::expr>::const_iterator it = edges_iso.begin();
+       it != edges_iso.end(); ++it) {
+    z3::expr expr = *it;
+    solver.add(expr);
+  }
+
+  return false;
+}
+
+void Iso::get_encoding(std::vector<z3::expr>& nodes_iso,
+                       std::vector<z3::expr>& edges_iso)
+{
   // 1. Declare encoding variables
 
   // 1. Encode constraints of iso_node_v0_v1
@@ -27,6 +50,7 @@ bool Iso::is_iso()
    *
    * iso_node_v0_v1 -> (label(v0) = label(v1))
    */
+  nodes_iso.clear();
   for (nodes_t::const_iterator it_a =  acdfg_a.begin_nodes();
        it_a != acdfg_a.end_nodes(); ++it_a) {
     Node& node_a = *(*it_a);
@@ -39,11 +63,11 @@ bool Iso::is_iso()
         z3::expr iso_var = get_iso_var(node_a, node_b);
 
         z3::expr iso_eq = get_iso_eq(node_a, node_b);
-        iso_encoding = iso_encoding && ((! iso_var) || iso_eq);
+
+        nodes_iso.push_back((! iso_var) || iso_eq);
       }
     }
   }
-
 
   // 3. Encode the constraints of iso_edge_e0_e1
   /* For all pairs of edges (that could match) iso_edge_e0_e1
@@ -55,10 +79,24 @@ bool Iso::is_iso()
    * WARNING label(e1) = label(e2) may be defined recursively on
    * other isomorphism variables for nodes
    */
+  edges_iso.clear();
+  for (edges_t::const_iterator it_a =  acdfg_a.begin_edges();
+       it_a != acdfg_a.end_edges(); ++it_a) {
+    Edge& edge_a = *(*it_a);
+    for (edges_t::const_iterator it_b =  acdfg_b.begin_edges();
+         it_b != acdfg_b.end_edges(); ++it_b) {
+      Edge& edge_b = *(*it_b);
 
-  return false;
+      if (may_match(edge_a, edge_b)) {
+        z3::expr iso_var = get_iso_var(edge_a, edge_b);
+
+        z3::expr iso_eq = get_iso_eq(edge_a, edge_b);
+
+        edges_iso.push_back((! iso_var) || iso_eq);
+      }
+    }
+  }
 }
-
 
 z3::expr Iso::get_iso_var(Node &n_a, Node &n_b) {
   char* var_name = get_var_name("iso_node_",
@@ -81,13 +119,28 @@ z3::expr Iso::get_iso_var(Edge &e_a,Edge &e_b)
 
 bool Iso::may_match(const Node& n_a, const Node& n_b)
 {
+  assert(false);
   return false;
 }
 
 z3::expr Iso::get_iso_eq(const Node& n_a, const Node& n_b)
 {
+  assert(false);
   return z3_context.bool_val(true);
 }
+
+bool Iso::may_match(const Edge& e_a, const Edge& e_b)
+{
+  assert(false);
+  return false;
+}
+
+z3::expr Iso::get_iso_eq(const Edge& e_a, const Edge& e_b)
+{
+  assert(false);
+  return z3_context.bool_val(true);
+}
+
 
 char* Iso::get_var_name(const char* prefix, long id1, long id2)
 {
--- src/fixrgraphiso/iso.h
+++ src/fixrgraphiso/iso.h
@@ -13,6 +13,8 @@
 
 namespace fixrgraphiso {
 
+
+
 class Iso {
 public:
   Iso(Acdfg& a, Acdfg& b) : acdfg_a(a), acdfg_b(b) {};
@@ -25,10 +27,14 @@ private:
   Acdfg& acdfg_b;
   z3::context z3_context;
 
+  void get_encoding(std::vector<z3::expr>& nodes_iso,
+                    std::vector<z3::expr>& edges_iso);
   z3::expr get_iso_var(Node &n_a, Node &n_b);
   z3::expr get_iso_var(Edge &e_a,Edge &e_b);
   bool may_match(const Node& n_a, const Node& n_b);
+  bool may_match(const Edge& e_a, const Edge& e_b);
   z3::expr get_iso_eq(const Node& n_a, const Node& n_b);
+  z3::expr get_iso_eq(const Edge& e_a, const Edge& e_b);
   char* get_var_name(const char* prefix, long id1, long id2);
   string get_str(long id);
 };
