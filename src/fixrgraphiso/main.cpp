d5eee415d8a897ae2e7f92374720056ab16161b3
smover <sergio.mover@gmail.com>
2016-06-14 00:13:25 -0600

    Add computation of isomorphism in the main (still to be tested).

--- src/fixrgraphiso/main.cpp
+++ src/fixrgraphiso/main.cpp
@@ -9,12 +9,13 @@
 
 #include "fixrgraphiso/serialization.h"
 #include "fixrgraphiso/acdfg.h"
+#include "fixrgraphiso/iso.h"
 
 using namespace fixrgraphiso;
 
-void clean(acdfg_protobuf::Acdfg* proto_acdfg_1,
-           acdfg_protobuf::Acdfg* proto_acdfg_2,
-           Acdfg* acdfg_1, Acdfg* acdfg_2);
+void clean(acdfg_protobuf::Acdfg* proto_acdfg_a,
+           acdfg_protobuf::Acdfg* proto_acdfg_b,
+           Acdfg* acdfg_a, Acdfg* acdfg_b);
 
 int main (int argc, char *argv[])
 {
@@ -24,58 +25,76 @@ int main (int argc, char *argv[])
   }
   else {
     AcdfgSerializer serializer;
-    acdfg_protobuf::Acdfg* proto_acdfg_1 = NULL;
-    acdfg_protobuf::Acdfg* proto_acdfg_2 = NULL;
-    Acdfg* acdfg_1 = NULL;
-    Acdfg* acdfg_2 = NULL;
+    acdfg_protobuf::Acdfg* proto_acdfg_a = NULL;
+    acdfg_protobuf::Acdfg* proto_acdfg_b = NULL;
+    Acdfg* acdfg_a = NULL;
+    Acdfg* acdfg_b = NULL;
 
-    proto_acdfg_1 = serializer.read_protobuf_acdfg(argv[1]);
-    if (NULL == proto_acdfg_1) {
+    proto_acdfg_a = serializer.read_protobuf_acdfg(argv[1]);
+    if (NULL == proto_acdfg_a) {
       std::cerr << "Error reading file " << argv[1] << "\n";
-      clean(proto_acdfg_1, proto_acdfg_2, acdfg_1, acdfg_2);
+      clean(proto_acdfg_a, proto_acdfg_b, acdfg_a, acdfg_b);
       return 1;
     }
 
-    proto_acdfg_2 = serializer.read_protobuf_acdfg(argv[2]);
-    if (NULL == proto_acdfg_2) {
+    proto_acdfg_b = serializer.read_protobuf_acdfg(argv[2]);
+    if (NULL == proto_acdfg_b) {
       std::cerr << "Error reading file " << argv[2] << "\n";
-      clean(proto_acdfg_1, proto_acdfg_2, acdfg_1, acdfg_2);
+      clean(proto_acdfg_a, proto_acdfg_b, acdfg_a, acdfg_b);
       return 1;
     }
 
-    acdfg_1 = serializer.create_acdfg(proto_acdfg_1);
-    if (NULL == acdfg_1) {
+    acdfg_a = serializer.create_acdfg(proto_acdfg_a);
+    if (NULL == acdfg_a) {
       std::cerr << "Error building the acdfg for " << argv[1] << "\n";
-      clean(proto_acdfg_1, proto_acdfg_2, acdfg_1, acdfg_2);
-      delete(acdfg_1);
+      clean(proto_acdfg_a, proto_acdfg_b, acdfg_a, acdfg_b);
+      delete(acdfg_a);
       return 1;
     }
 
-    acdfg_2 = serializer.create_acdfg(proto_acdfg_2);
-    if (NULL == acdfg_2) {
+    acdfg_b = serializer.create_acdfg(proto_acdfg_b);
+    if (NULL == acdfg_b) {
       std::cerr << "Error building the acdfg for " << argv[2] << "\n";
-      clean(proto_acdfg_1, proto_acdfg_2, acdfg_1, acdfg_2);
-      delete(acdfg_2);
+      clean(proto_acdfg_a, proto_acdfg_b, acdfg_a, acdfg_b);
+      delete(acdfg_b);
       return 1;
     }
 
-    std::cout << "Acdfg 1\n" << (*acdfg_1);
-    std::cout << "\nAcdfg 2\n" << (*acdfg_2);
+    std::cout << "Acdfg a\n" << (*acdfg_a);
+    std::cout << "\nAcdfg b\n" << (*acdfg_b);
 
-    clean(proto_acdfg_1, proto_acdfg_2, acdfg_1, acdfg_2);
+    /* Compute the isomorphism */
+    {
+      IsoSolver solver(*acdfg_a, *acdfg_b);
+
+      /* compute (precise) isomorphism */
+      bool is_iso = solver.is_iso();
+      std::cout << "a is isomorphic to b? " << is_iso;
+      if (is_iso) {
+        std::cout << solver.get_last_isomorphism();
+      }
+
+      bool is_approx_iso = solver.get_max_embedding();
+      std::cout << "a is isomorphic to b? " << is_approx_iso;
+      if (is_approx_iso) {
+        std::cout << solver.get_last_isomorphism();
+      }
+    }
+
+    clean(proto_acdfg_a, proto_acdfg_b, acdfg_a, acdfg_b);
   }
 
   return 0;
 }
 
-void clean(acdfg_protobuf::Acdfg* proto_acdfg_1,
-           acdfg_protobuf::Acdfg* proto_acdfg_2,
-           Acdfg* acdfg_1, Acdfg* acdfg_2)
+void clean(acdfg_protobuf::Acdfg* proto_acdfg_a,
+           acdfg_protobuf::Acdfg* proto_acdfg_b,
+           Acdfg* acdfg_a, Acdfg* acdfg_b)
 {
-  if (NULL != proto_acdfg_1) delete proto_acdfg_1;
-  if (NULL != proto_acdfg_2) delete proto_acdfg_2;
-  if (NULL != acdfg_1) delete acdfg_1;
-  if (NULL != acdfg_2) delete acdfg_2;
+  if (NULL != proto_acdfg_a) delete proto_acdfg_a;
+  if (NULL != proto_acdfg_b) delete proto_acdfg_b;
+  if (NULL != acdfg_a) delete acdfg_a;
+  if (NULL != acdfg_b) delete acdfg_b;
 
   // Optional:  Delete all global objects allocated by libprotobuf.
   google::protobuf::ShutdownProtobufLibrary();
