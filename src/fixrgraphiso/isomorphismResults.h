d4e73a21f777751a098c2b9f548f0fbd25003b5c
Rhys Braginton Pettee Olsen <rhol9958@colorado.edu>
2016-08-03 22:56:28 -0400

    Add helper method to get pointer to Protobuf

--- src/fixrgraphiso/isomorphismResults.h
+++ src/fixrgraphiso/isomorphismResults.h
@@ -1,9 +1,13 @@
+#ifndef D__ISORESULTS__HH__
+#define D__ISORESULTS__HH__
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include "fixrgraphiso/acdfg.h"
+#include "fixrgraphiso/proto_iso.pb.h"
 
-#ifndef D__ISORESULTS__HH__
-#define D__ISORESULTS__HH__
+namespace iso_protobuf = edu::colorado::plv::fixr::protobuf;
 
 namespace fixrgraphiso {
 
@@ -12,17 +16,17 @@ namespace fixrgraphiso {
     long b_id; // ID for graph b
     double wt; // weight is only relevant for node pairs.
   };
-  
+
   class IsomorphismResults{
   public:
     std::vector<iso> isoNodes; // Information about nodes
     std::vector<iso> isoEdges; // Information about edges
     std::string graphA;
     std::string graphB;
-
+    
   IsomorphismResults(std::string aName, std::string bName):graphA(aName), graphB(bName)
     {};
-
+    
     void addIsoNodePair(long node_a_id, long node_b_id, double weight){
       iso istr;
       istr.a_id = node_a_id;
@@ -39,10 +43,36 @@ namespace fixrgraphiso {
       isoEdges.push_back(istr);
     }
 
-    void dumpProtobuf();
-    
-  };
+    iso_protobuf::Iso * toProtobuf() {
+      iso_protobuf::Iso * proto = new iso_protobuf::Iso();
 
+      std::vector<iso>::iterator it;
+
+      // add node maps
+      for (it = isoNodes.begin(); it < isoNodes.end(); ++it) {
+	iso_protobuf::Iso_MapNode * map_node = proto->add_map_node();
+	map_node->set_id_1(it->a_id);
+	map_node->set_id_2(it->b_id);
+	map_node->set_weight(it->wt);
+      }
+
+      // add edge maps
+      for (it = isoEdges.begin(); it < isoEdges.end(); ++it) {
+	iso_protobuf::Iso_MapNode * map_node = proto->add_map_node();
+	map_node->set_id_1(it->a_id);
+	map_node->set_id_2(it->b_id);
+	map_node->set_weight(it->wt);
+      }
+      
+      proto->set_graph_1_id(graphA);
+      proto->set_graph_2_id(graphB);
+
+      return proto;
+    }
+
+    void dumpProtobuf();
+  
+  };
 }
 
 #endif
