7aa41f94f37c59fb4149174691f1d101d97de0bf
smover <sergio.mover@gmail.com>
2017-02-10 18:26:27 -0700

    get pattern stats

--- scripts/processGroum.py
+++ scripts/processGroum.py
@@ -2,6 +2,9 @@
 
 import os
 import re
+import sys
+
+from gatherResults import GenerateIndexPage
 
 # Collect the statistics of a pattern
 class PatternStats:
@@ -133,3 +136,150 @@ class PatternStats:
                 continue
 
         return patterns
+
+
+def get_from_dot(dotFileName):
+    f = open(dotFileName, "rt")
+
+    size = 0
+    method_bag = []
+
+    for line in f:
+        line = line.strip()
+
+        if ("shape=box" in line):
+            size = size + 1
+
+            m = re.match(r'.*label="(.*)\[(.*)', line)
+            if m:
+                method_name = m.group(1)
+                method_name = method_name.strip()
+                # TODO FIX INIT
+                method_bag.append(method_name)
+
+
+        elif ("shape=ellipse" in line):
+            size = size + 1
+
+        m = re.match(r':', line)
+        if m:
+            continue
+
+    return (size, method_bag)
+
+def parseInfoFile(cluster_folder, clusterID, type=1):
+    filename = '%s/cluster_%d/cluster_%d_info.txt'%(cluster_folder, clusterID, clusterID)
+
+    patterns = []
+
+    try:
+        f = open(filename, 'rt')
+        patternList = []
+        patternType = 1
+        patternID = -1
+        patternFrequency = -1
+        dotFilename = None
+        for line in f:
+            line = line.strip()
+            m = re.match(r'Popular\s*Bins:', line)
+            if m:
+                continue
+
+            m = re.match(r'(\w+)\s*Bin\s*#\s*(\d+)', line)
+            if m:
+                if (patternID >= 0):
+                    if (patternType == type):
+                        dotfile = '%s/cluster_%d/%s'%(cluster_folder, clusterID, dotFileName)
+                        (size, method_bag) = get_from_dot(dotfile)
+
+                        pattern = PatternStats(clusterID, patternID,
+                                               size,
+                                               patternFrequency,
+                                               method_bag,
+                                               patternList)
+                        patterns.append(pattern)
+                    patternList=[]
+                    dotFileName = None
+                    patternFrequency=None
+                patternDescr = m.group(1)
+                if (patternDescr == 'Popular'):
+                    patternType = 1
+                elif (patternDescr == 'Anomalous'):
+                    patternType = 2
+                else:
+                    patternType = 3
+                patternID = int(m.group(2))
+                continue
+            m = re.match(r'Dot:\s*(.*.dot)', line)
+            if m:
+                dotFileName = m.group(1)
+                continue
+            # Otherwise it is a filename
+            m = re.match(r'Frequency\s*:\s*(\d+),\s*(\d+)', line)
+            if m:
+                patternFrequency = int(m.group(1)) + int(m.group(2))
+                continue
+            m = re.match(r'Frequency\s*:\s*(\d+)(.*)$',line)
+            if m:
+                patternFrequency = int(m.group(1))
+                patternList.append(m.group(2))
+                continue
+            patternList.append(line)
+
+        f.close()
+
+    except IOError:
+        print('Error: could not open file --', filename)
+
+    return patterns
+
+
+def read_graphiso_patterns(folder, id):
+    patterns = parseInfoFile(folder, id)
+
+    return patterns
+
+
+# TODO:
+# - read single dot
+# - read get all the dot_i_pat.dot
+# - get_all the patterns4graphs
+#
+# - DUMP:
+# -  - histogram of pattern sizes
+# -  - numbers for subsumptions
+#
+
+def main(argv):
+
+    # hardcode everything
+    graph_iso_folder="/home/sergio/works/projects/muse/repos/FixrGraph_experiments/msr_results/graphiso"
+    groum_folder="/home/sergio/works/projects/muse/repos/FixrGraph_experiments/msr_results/grouminer"
+
+    "Map from cluster id to pattern list"
+    graphiso_patterns = {}
+    groum_patterns = {}
+
+    # Gather the patterns
+    for id in range(1, 2):
+
+        # read the graphiso pattern
+        graphiso_patterns[id] = read_graphiso_patterns(os.path.join(graph_iso_folder, "all_clusters"), id)
+
+        # read the grouminer data
+        groum_f_name = os.path.join(groum_folder, "all_clusters", "cluster_%d"%id, "patterns4graph.txt")
+        if (os.path.exists(groum_f_name)):
+            groum_patterns[id]  = PatternStats.readFromGroums(groum_f_name, id)
+        else:
+            print "Missing file %s" % groum_f_name
+
+    # 1. Compare the patterns
+    print groum_patterns
+    print graphiso_patterns
+
+    # 2. Print the histogram file
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
