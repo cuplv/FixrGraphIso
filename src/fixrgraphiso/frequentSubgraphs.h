e2fd4b9f5e92f3f0ea52584e4f1dd3f191e9641f
smover <sergio.mover@gmail.com>
2018-08-21 17:29:30 -0600

    Separate frequent subgraph code in a class

---
+++ src/fixrgraphiso/frequentSubgraphs.h
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <vector>
+#include "fixrgraphiso/acdfgBin.h"
+
+namespace fixrgraphiso {
+  using std::string;
+  using std::vector;
+
+  class FrequentSubgraphMiner {
+    private:
+
+    protected:
+    void processCommandLine(int argc, char * argv[],
+                            vector<string> & filenames,
+                            vector<string> & methodNames);
+
+    void calculateLatticeGraph(vector<AcdfgBin*> & allBins);
+
+    void classifyBins(vector<AcdfgBin*> & allBins,
+                      vector<AcdfgBin*> & popular,
+                      vector<AcdfgBin*> & anomalous,
+                      vector<AcdfgBin*> & isolated);
+
+    void computePatternsThroughSlicing(vector<string> & filenames,
+                                       vector<string> & methodnames);
+
+    void testPairwiseSubsumption(vector<string> & filenames,
+                                 vector<string> & methodnames);
+
+    void dumpAllBins(vector<AcdfgBin*> & popular,
+                     vector<AcdfgBin*> & anomalous,
+                     vector<AcdfgBin*> & isolated,
+                     std::chrono::seconds time_taken,
+                     const string & output_prefix,
+                     const string & infoFileName);
+
+
+    public:
+    FrequentSubgraphMiner();
+    void mine(int argc, char * argv []);
+
+
+
+    private:
+    int freq_cutoff = 20;
+    string info_file_name = "cluster-info.txt";
+    string output_prefix = ".";
+    int minTargetSize = 2;
+    int maxTargetSize = 100;
+    int maxEdgeSize = 400;
+    int anomalyCutOff = 5;
+    bool runTestOfSubsumption = false;
+  };
+
+
+}
